5770e4b2f28f2ccebd3a249e3eb691aa19fcd1bc
Guillermo Cruz <guillermo.951113@gmail.com>
2021-02-04 03:19:58 -0600

    button to navigate between activities

--- app/src/main/java/com/guillermo/accelerometer/HomeActivity.kt
+++ app/src/main/java/com/guillermo/accelerometer/HomeActivity.kt
@@ -1,11 +1,20 @@
 package com.guillermo.accelerometer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        monitoringAccelerations.setOnClickListener {
+            val goToMonitoring = Intent(this, ChartsActivity::class.java)
+            startActivity(goToMonitoring)
+        }
     }
+
+
 }
