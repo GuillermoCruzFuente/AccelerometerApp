5d205ab0d74b8011cd54c9baeec880083c2a0194
Guillermo Cruz <guillermo.951113@gmail.com>
2021-02-04 23:01:42 -0600

    Specific class RealTimeChart.kt

--- app/src/main/java/com/guillermo/accelerometer/ChartsActivity.kt
+++ app/src/main/java/com/guillermo/accelerometer/ChartsActivity.kt
@@ -6,21 +6,23 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import kotlinx.android.synthetic.main.activity_charts.*
 
 class ChartsActivity : AppCompatActivity(), SensorEventListener{
     private var mSensorManager: SensorManager? = null
     private var mAccelerometer: Sensor? = null
-    private var mChart: LineChart? = null
+
     private var thread: Thread? = null
     private var plotData = true
+
+//    private var chartX: LineChart? = null
+//    private var chartY: LineChart? = null
+//    private var chartZ: LineChart? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,46 +33,47 @@ class ChartsActivity : AppCompatActivity(), SensorEventListener{
         if (mAccelerometer != null) {
             mSensorManager!!.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST)
         }
-        mChart = findViewById<View>(R.id.chartY) as LineChart
+
+//        chartX = findViewById<View>(R.id.chartY) as LineChart
 
         // enable description text
-        mChart!!.description.isEnabled = false
+        chartX!!.description.isEnabled = false
 
         // enable touch gestures
-        mChart!!.setTouchEnabled(false)
+        chartX!!.setTouchEnabled(false)
 
         // enable scaling and dragging
-        mChart!!.isDragEnabled = false
-        mChart!!.setScaleEnabled(false)
-        mChart!!.setDrawGridBackground(false)
+        chartX!!.isDragEnabled = false
+        chartX!!.setScaleEnabled(false)
+        chartX!!.setDrawGridBackground(false)
 
         // if disabled, scaling can be done on x- and y-axis separately
-        mChart!!.setPinchZoom(false)
+        chartX!!.setPinchZoom(false)
 
         // set an alternative background color
-        mChart!!.setBackgroundColor(Color.GRAY)
+        chartX!!.setBackgroundColor(Color.GRAY)
         val data = LineData()
         data.setValueTextColor(Color.LTGRAY)
 
         // add empty data
-        mChart!!.data = data
+        chartX!!.data = data
 
         // get the legend (only possible after setting data)
-        val l = mChart!!.legend
+        val l = chartX!!.legend
         l.isEnabled = false
 
         // modify the legend ...
 //        l.form = Legend.LegendForm.LINE
 //        l.textColor = Color.WHITE
 
-        val xl = mChart!!.xAxis
+        val xl = chartX!!.xAxis
 //        xl.textColor = Color.WHITE
 //        xl.setDrawGridLines(true)
 //        xl.setAvoidFirstLastClipping(true)
 //        xl.isEnabled = true
         xl.isEnabled = false
 
-        val leftAxis = mChart!!.axisLeft
+        val leftAxis = chartX!!.axisLeft
         leftAxis.isEnabled = true
         leftAxis.textColor = Color.LTGRAY
         leftAxis.setDrawGridLines(true)
@@ -79,16 +82,16 @@ class ChartsActivity : AppCompatActivity(), SensorEventListener{
 
         leftAxis.setDrawGridLines(false)
 //        leftAxis.setDrawGridLines(true)
-        val rightAxis = mChart!!.axisRight
+        val rightAxis = chartX!!.axisRight
         rightAxis.isEnabled = false
-        mChart!!.axisLeft.setDrawGridLines(false)
-        mChart!!.xAxis.setDrawGridLines(false)
-        mChart!!.setDrawBorders(false)
+        chartX!!.axisLeft.setDrawGridLines(false)
+        chartX!!.xAxis.setDrawGridLines(false)
+        chartX!!.setDrawBorders(false)
         feedMultiple()
     }
 
     private fun addEntry(event: SensorEvent) {
-        val data = mChart!!.data
+        val data = chartX!!.data
         if (data != null) {
             var set = data.getDataSetByIndex(0)
             // set.addEntry(...); // can be called as well
@@ -103,14 +106,14 @@ class ChartsActivity : AppCompatActivity(), SensorEventListener{
             data.notifyDataChanged()
 
             // let the chart know it's data has changed
-            mChart!!.notifyDataSetChanged()
+            chartX!!.notifyDataSetChanged()
 
             // limit the number of visible entries
-            mChart!!.setVisibleXRangeMaximum(500f)
+            chartX!!.setVisibleXRangeMaximum(500f)
             // mChart.setVisibleYRange(30, AxisDependency.LEFT);
 
             // move to the latest entry
-            mChart!!.moveViewToX(data.entryCount.toFloat())
+            chartX!!.moveViewToX(data.entryCount.toFloat())
         }
     }
 
